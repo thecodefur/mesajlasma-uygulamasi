02e33d66a169fb4072c7ee90e6542d8dc774b5a3
Coder Furkan <devofur@gmail.com>
2023-06-18 17:05:37 +0300

    proje yükleme

---
+++ mesajlasma_uygulamasi/IService1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace mesajlasma_uygulamasi
+{
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string MesajEkIe(string value);
+
+        [OperationContract]
+        string MesajGetir();
+
+        [OperationContract]
+        void MesajlarıSil();
+
+    }
+
+
+    // Hizmet işlemlerine bileşik türler eklemek için, aşağıdaki örnekte gösterildiği şekilde bir veri sözleşmesi kullanın.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ mesajlasma_uygulamasi/Service1.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace mesajlasma_uygulamasi
+{
+    // NOT: "Service1" sınıf adını kodda, svc'de ve yapılandırma dosyasında birlikte değiştirmek için "Yeniden Düzenle" menüsündeki "Yeniden Adlandır" komutunu kullanabilirsiniz.
+    // NOT: Bu hizmeti test etmek üzere WCF Test İstemcisi'ni başlatmak için lütfen Çözüm Gezgini'nde Service1.svc'yi veya Service1.svc.cs'yi seçin ve hata ayıklamaya başlayın.
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+
+
+    public class Service1 : IService1
+    {
+        string mesajlar = "";
+
+        public string MesajEkIe(string value)
+        {
+            mesajlar += value + "\n";
+            return mesajlar;
+        }
+
+        public string MesajGetir()
+        {
+            return mesajlar;
+        }
+
+        public void MesajlarıSil()
+        {
+            mesajlar = "";
+        }
+
+    }
+}
